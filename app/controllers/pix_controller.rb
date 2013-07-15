4e4396414a29a15622814588a2234d09fd2d7bbe
dhagan <dhagan@yahoo.com>
2013-07-15 09:25:30 -1000

    (r)url aka rails curl, parses post for now.

--- app/controllers/pix_controller.rb
+++ app/controllers/pix_controller.rb
@@ -5,18 +5,48 @@ class PixController < ApplicationController
     puts "on lookup for " + params[:id]
     @patients = User.all
 
-    client = Savon.client(wsdl: "http://web03:8080/axis2/services/OrderService?wsdl" )
-    
-    puts "available ops: "
-    client.operations.each do |ops|
-      puts ops
-    end
-    puts " ~~~~ "
+#   client = Savon.client(wsdl: "http://web03:8080/axis2/services/OrderService?wsdl" )
+#    
+#    puts "available ops: "
+#    client.operations.each do |ops|
+#      puts ops
+#    end
+#    puts " ~~~~ "
     #result = client.get();
-    
     respond_to do |format|
       format.xml { render :xml=>@patients } 
       format.json { render :json=>@patients } 
+    end
+  end
+  #
+  # /pix/rurl - (r)uby curl
+  # @return
+  # @param
+  #
+  #require 'REXML/document'
+  require 'nokogiri'
+
+  def rurl
+    retXML = request.body.read;
+    logger.debug 'Hello world!'
+    #logger.debug request.body.read
+    #@patients = User.all
+    # extract event information
+    doc = REXML::Document.new(retXML)
+    patientIds = []
+    links = []
+    doc.elements.each('/Order/subject/reference') do |ele|
+      patientIds << ele
+    end
+    @doc =Nokogiri::XML(retXML)
+    #logger.debug @doc.to_s
+    #logger.debug @doc.xpath('/Order')     # doesn't work
+    logger.debug @doc.css('reference').first['value']
+    logger.debug patientIds
+
+    respond_to do |format|
+      format.xml { render :xml=>retXML}
+      #format.json { render :json=>@patients }
     end
   end
 
