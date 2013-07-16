aa846e558b5de97e2b777676b7fda72dbeef92e1
dhagan <dhagan@yahoo.com>
2013-07-15 16:51:14 -1000

    cleanup of pix request headed through processing of medication

--- app/controllers/pixRequestCL.rb
+++ app/controllers/pixRequestCL.rb
@@ -1,52 +1,20 @@
-#require 'soap/wsdlDriver'
+require 'nokogiri'
 require 'savon'
 
-=begin
-   module DBLogger
-     def self.debug(message)
-       p message  # replace with custom impl.
-     end
-   end
+filename = "FIHRRXOrder.xml"
+fileXML = File.read(filename)
+@requestXMLDoc =Nokogiri::XML(fileXML)
+patientId = @requestXMLDoc.css('reference').first['value']
+ puts patientId
 
-   Savon.configure do |config|
-     config.logger = DBLogger
-   end
-
-   Savon.configure do |c|
-     c.log = true
-   end
-=end
-
-#client = Savon.client(wsdl: "http://172.16.12.82:37080/axis2/services/xdsbridge?wsdl" )
-
-#filename = 'PRPA_IN201309UV02.xml'
-filename = "PIXSoapEnv.xml"
+filename = "PIXRequestSoapEnv.xml"
 file_content = File.read(filename)
 @xmldoc = Nokogiri::XML(file_content)
 
+#  TODO munge the soap request xml to use the patient id and or firstname, last name from above
 wsdl = "http://172.16.12.82:37080/axis2/services/pixmgr?wsdl"
 endpoint = "http://172.16.12.82:37080/axis2/services/pixmgr"
 content_type = '"application/soap+xml;charset=UTF-8;action="urn:hl7-org:v3:PRPA_IN201309UV02"'
-#namespace = "http://serviceimpl.pixpdqv3.services.hieos.vangent.com"
-=begin
-   wsdl = "http://172.16.12.82:37080/axis2/services/pixmgr?wsdl"
-
-   namespaces = {
-       "xmlns:soap" => "http://www.w3.org/2003/05/soap-envelope",
-       "xmlns:ser" => "http://serviceimpl.pixpdq.services.hieos.vangent.com",
-   }
-
-   soap_header ={
-       "wsa:Action" => "urn:hl7-org:v3:PRPA_IN201309UV02",
-       }
-
-   client = Savon.client(wsdl: wsdl,
-                         namespaces: namespaces,
-                         soap_header: soap_header,
-                         pretty_print_xml: true
-
-   )
-=end
 client = Savon.client(wsdl: wsdl,
                       endpoint: endpoint,
                       headers: {
@@ -64,26 +32,8 @@ puts " ------------------------ "
 #response = client.call(:patient_registry_get_identifiers_query, message: { id: 42 })
 response = client.call(:patient_registry_get_identifiers_query, xml: @xmldoc.to_s)
 
-=begin
-      response = client.call(:patient_registry_get_identifiers_query) do
-        message username: "luke", password: "secret"
-      end
-
-      pp response.body.to_hash
-=end
-
+# TODO extract the patient and shove it back into the FIHRRxOrder.xml to form the response back to the message flow
 
 #rescue Savon::SOAP::Fault => error
 #print error
 #end
-=begin
-   response = client.request(:mes, "login") do
-     soap.body = {
-         "mes:Username" => "test",
-         "mes:Password" => "test",
-         "mes:ImpersonationUsername"=>"Test",
-         "mes:ApplicationName"=>"test"
-     }
-   end
-   pp response.to_hash
-=end
