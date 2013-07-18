9dd11e58fcb3780866bfcef68ffea258c93197a5
dhagan <dhagan@yahoo.com>
2013-07-17 15:46:39 -1000

    added medication controller

---
+++ app/controllers/medication_controller.rb
@@ -0,0 +1,74 @@
+require 'net/http'
+require 'uri'
+require 'nokogiri'
+
+class MedicationController < ApplicationController
+  def rurl
+    requestBodyXML = request.body.read;
+    logger.debug 'Hello world!'
+
+    @requestXMLDoc = Nokogiri::XML(requestBodyXML)
+    patientId = @requestXMLDoc.css('/rtop2/soaData/patient')
+
+
+#
+# assemble medication input xml
+#
+
+    medicationBuilder = Nokogiri::XML::Builder.new do |xml|
+      xml.Patient {
+        xml.ids {
+          xml.id_ patientId[0]['ien'].to_s
+          xml.system patientId[0]['system'].to_s
+        }
+        #xml.ids {
+        #  xml.id_ patientId[1]['ien'].to_s
+        #  xml.system patientId[1]['system'].to_s
+        #}
+
+      }
+    end
+
+#
+# call patient history lookup
+#
+    url = URI.parse('http://10.255.166.15:8080/patienthistory/webresources/patient-history-lookup/multiple')
+    request = Net::HTTP::Post.new(url.path)
+    request.content_type = 'application/xml'
+
+    request.body = medicationBuilder.to_xml
+    response = Net::HTTP.start(url.host, url.port) { |http| http.request(request) }
+    puts 'response ---------'
+    cleanResponse = response.body.to_s[1..-1].chomp(']')
+
+
+#
+# TODO extract medications
+#
+    firstXML, *lastXML = cleanResponse.split(/, /)
+    puts firstXML
+    puts *lastXML
+
+    @firstXML = Nokogiri::XML(firstXML)
+    @firstXML.remove_namespaces!
+
+#
+# insert medications into the FIHRRxOrder.xml to form the response back to the message flow
+#
+
+    soaData = @requestXMLDoc.at_css "soaData"
+    medicationHistoryComment = Nokogiri::XML::Comment.new @requestXMLDoc, ' Medication history from endpoint/patienthistory/webresources/patient-history-lookup  '
+    soaData.add_child(medicationHistoryComment)
+    medication = Nokogiri::XML::Node.new "medication", @requestXMLDoc
+    medication['name']= 'aspirin'
+    medication['code']= '123456'
+    soaData.add_child(medication)
+
+    respond_to do |format|
+      format.xml { render :xml => @requestXMLDoc }
+      #format.json { render :json=>@patients }
+    end
+  end
+
+
+end
