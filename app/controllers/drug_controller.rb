ee477980c1f37e2422ed23a6b1985cd2e464617b
Billy Cao <caoabunga@me.com>
2013-08-01 13:10:29 -1000

    adding regex matching on drug controller for checking interaction result

--- app/controllers/drug_controller.rb
+++ app/controllers/drug_controller.rb
@@ -78,7 +78,8 @@ class DrugController < ApplicationController
       #
       # Push failure to /monitoring
       #
-      if (cleanResponse != "No Interactions Found")
+      if (/#{"(?i:n)o (?i:i)nteractions (?i:f)ound"}/.match(cleanResponse))
+        logger.debug("No interactions were found: " + cleanResponse)
         filename = File.join(Rails.root, 'app', 'controllers', drugdrugInteractionOrderResponseFileName)
         fileXML = File.read(filename)
         @drugDrugInteractionResponseXMLDoc = Nokogiri::XML(fileXML)
